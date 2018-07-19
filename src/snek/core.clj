0ef7abd693a15a9adbe7f23f183bbd7b2c87d5ad
patrick.hildreth <patrick.hildreth@patrick-hildreth-mbp.local>
2018-07-19 12:55:52 -0500

    added shells of potential funcitons

--- src/snek/core.clj
+++ src/snek/core.clj
@@ -31,17 +31,38 @@
 (def d-length 3)
 (def p-width (* c-width point-size))
 (def p-height (* c-height point-size))
-(def-with-docs directions "Using Java KeyEvent Class. VK indicates virtual key codes.  https://docs.oracle.com/javase/7/docs/api/java/awt/event/KeyEvent.html for more info"
+(def-with-docs directions "Using Java KeyEvent Class. VK indicates virtual key codes. See https://docs.oracle.com/javase/7/docs/api/java/awt/event/KeyEvent.html for more info"
   {KeyEvent/VK_LEFT [-1 0]
    KeyEvent/VK_RIGHT [1 0]
    KeyEvent/VK_UP [0 -1]
    KeyEvent/VK_DOWN [0 1]})
 
-(def color-variation 35)
-(def bright-sum 350)
-(def bright-diff 250)
-(def background-color (Color/WHITE))
-(def text-color (Color/DARK_GRAY))
+(defn quantum [level]
+  (max (+ i-length (* level d-length)) m-quantum))
+
+(defn length [level]
+  (+ i-length (* level d-length)))
+
+(def length (memorize length))
+
+(defn add-points [])
+
+(defn move [])
+
+(defn turn [])
+
+(defn win? [])
+
+(defn eats-self? [])
+
+(defn eats-boarder? [])
+
+(defn lose? [])
+
+(defn eats-blip? [])
+
+(defn screen-rect [])
+
 
 (defn -main
   "I don't do a whole lot ... yet."
